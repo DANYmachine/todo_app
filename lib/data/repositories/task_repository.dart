86e66c40929fcf407630276d1e0f3c8cf8f9a71c
DANYmachine <61663613+DANYmachine@users.noreply.github.com>
2023-05-24 11:53:27 +0300

    primary structure for data layer

---
+++ lib/data/repositories/task_repository.dart
@@ -0,0 +1,8 @@
+import '../models/task_model.dart';
+
+abstract class TaskRepository {
+  Future<List<Task>> getTasks();
+  Future<void> addTask(Task task);
+  Future<void> updateTask(Task task);
+  Future<void> deleteTask(Task task);
+}
---
+++ lib/data/repositories/task_repository_impl.dart
@@ -0,0 +1,29 @@
+import 'package:todo_app/data/models/task_model.dart';
+
+import 'task_repository.dart';
+
+class TaskRepositoryImpl implements TaskRepository {
+  @override
+  Future<void> addTask(Task task) {
+    // TODO: implement addTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteTask(Task task) {
+    // TODO: implement deleteTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Task>> getTasks() {
+    // TODO: implement getTasks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateTask(Task task) {
+    // TODO: implement updateTask
+    throw UnimplementedError();
+  }
+}
